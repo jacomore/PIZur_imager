9ce4f9481dbc333ee631ce1e2367a17a74ad18da
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-02-02 18:06:42 +0100

    add destination of motion and control on input

--- control_PI.py
+++ control_PI.py
@@ -1,63 +1,54 @@
 from pipython import GCSDevice,pitools
-import warnings
+
+# declaring class for new exception
+class InvalidPosInput(Exception):
+    "Raised when the input value is out of range"
+    pass
+
+# checking whether input is correct 
+def pos_in_range(pos,rangemin,rangemax):
+    if pos <= rangemin or  pos >= rangemax:
+        raise InvalidPosInput
+
 
 CONTROLLERNAME = 'C-663'
 STAGES = 'L-406.40SD00'  # connect stages to axes
 #REFMODES = 'FPL'
 REFMODES = ('FNL')
+DESTINATION = 120 # mm
 
-while True:
+with GCSDevice(devname = CONTROLLERNAME) as pidevice: 
+    # return list of string with the found devices
+    devices = pidevice.EnumerateUSB(mask=CONTROLLERNAME)
+    # check that devices is not empty        
+    if not devices: 
+       raise Exception("There are no connected devices! Please, connect at least one device.")
+
+    # printout found devices
+    for i, device in enumerate(devices):
+        print('Number ---- Device')
+        print('{}      ----  {}'.format(i,device))
+
+    # read the selected device
+    item = int(input('Input the index of the device to connect:'))
+    pidevice.ConnectUSB(devices[item])
+    print('connected: {}'.format(pidevice.qIDN().strip()))
+
+    # initialise the controller and the stages
+    pitools.startup(pidevice, stages = STAGES, refmodes = REFMODES)
+
+    # return values of the minimum and maximum position of the travel range of axis
+    rangemin = list(pidevice.qTMN().values())
+    rangemax = list(pidevice.qTMX().values())
+
     try:
-        start = int(input("Please, input the starting point: "))
-        end = int(input("Please, input the ending point: "))
-        step = int(input("Please, input the step of the scan"))
-        break
+        is_valid = pos_in_range(DESTINATION,rangemin[0],rangemax[0])
     except ValueError:
-        print("Oops!  That was no valid number.  Try again...")
-
-def main():
-    """Connect controller and stage and display messages if successful."""
-    with GCSDevice(devname = CONTROLLERNAME) as pidevice: 
-        
-        # return list of string with the found devices
-        devices = pidevice.EnumerateUSB(mask=CONTROLLERNAME)
-
-        # check that devices is not empty        
-        if not devices: 
-            with warnings.catch_warnings():
-                warnings.simplefilter('error',category=Warning)
-                warnings.warn("There are no connected devices! Please, connect at least one device.")
-
-        # printout found devices
-        for i, device in enumerate(devices):
-            print('Number ---- Device')
-            print('{}      ----  {}'.format(i,device))
-
-        # read the selected device
-        item = int(input('Input the index of the device to connect:'))
-        pidevice.ConnectUSB(devices[item])
-        print('connected: {}'.format(pidevice.qIDN().strip()))
-
-        # initialise the controller and the stages
-        pitools.startup(pidevice, stages = STAGES, refmodes = REFMODES)
-
-        # return values of the minimum and maximum position of the travel range of axis
-        rangemin = list(pidevice.qTMN().values())
-        rangemax = list(pidevice.qTMX().values())
-
-        if start < rangemin:
-            with warnings.catch_warnings():
-                warnings.simplefilter('error',category=Warning)
-                warnings.warn("Input")
+        print("That was no valid number!")
+    except TypeError:
+        print("That was no valid type!")
+    except InvalidPosInput:
+        print("Desired destination is out of range!")
 
 
 
-if __name__ == '__main__':
-    # To see what is going on in the background you can remove the following
-    # two hashtags. Then debug messages are shown. This can be helpful if
-    # there are any issues.
-
-    #import logging
-    #logging.basicConfig(level=logging.DEBUG)
-
-    main()
