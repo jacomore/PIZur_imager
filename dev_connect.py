ce055b9c1054f2f8d1e3ab35d46ea2b25ae685a5
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-02-02 12:05:32 +0100

    added input interface for positions and step

--- dev_connect.py
+++ dev_connect.py
@@ -6,18 +6,27 @@ STAGES = 'L-406.40SD00'  # connect stages to axes
 #REFMODES = 'FPL'
 REFMODES = ('FNL')
 
+while True:
+    try:
+        start = int(input("Please, input the starting point: "))
+        end = int(input("Please, input the ending point: "))
+        step = int(input("Please, input the step of the scan"))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+
 def main():
     """Connect controller and stage and display messages if successful."""
     with GCSDevice(devname = CONTROLLERNAME) as pidevice: 
         
         # return list of string with the found devices
         devices = pidevice.EnumerateUSB(mask=CONTROLLERNAME)
-        
+
+        # check that devices is not empty        
         if not devices: 
             with warnings.catch_warnings():
                 warnings.simplefilter('error',category=Warning)
                 warnings.warn("There are no connected devices! Please, connect at least one device.")
-
 
         # printout found devices
         for i, device in enumerate(devices):
@@ -25,12 +34,22 @@ def main():
             print('{}      ----  {}'.format(i,device))
 
         # read the selected device
-        item = int(input('Input number of the device to connect:'))
+        item = int(input('Input the index of the device to connect:'))
         pidevice.ConnectUSB(devices[item])
         print('connected: {}'.format(pidevice.qIDN().strip()))
 
         # initialise the controller and the stages
         pitools.startup(pidevice, stages = STAGES, refmodes = REFMODES)
+
+        # return values of the minimum and maximum position of the travel range of axis
+        rangemin = list(pidevice.qTMN().values())
+        rangemax = list(pidevice.qTMX().values())
+
+        if start < rangemin:
+            with warnings.catch_warnings():
+                warnings.simplefilter('error',category=Warning)
+                warnings.warn("Input")
+
 
 
 if __name__ == '__main__':
