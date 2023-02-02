7dcee8b09ba6e9aa893250884f42fdec458b5b98
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-02-02 18:53:37 +0100

    added motion toward references

--- control_PI.py
+++ control_PI.py
@@ -1,21 +1,31 @@
-from pipython import GCSDevice,pitools
+from pipython import GCSDevice,pitools, GCS2Commands
 
 # declaring class for new exception
 class InvalidPosInput(Exception):
     "Raised when the input value is out of range"
     pass
 
-# checking whether input is correct 
+# check input is correct 
 def pos_in_range(pos,rangemin,rangemax):
     if pos <= rangemin or  pos >= rangemax:
         raise InvalidPosInput
 
 
+def move_to_ref(pidevice,REFMODES):
+    """move the stage towards the reference point"""
+    if REFMODES == 'FNL':
+        pidevice.FNL()
+    elif REFMODES == 'FPL':
+        pidevice.FPL()
+    pitools.waitontarget(pidevice)
+    print("Stage: {}".format(GCS2Commands.qCST(pidevice)['1']),"successfully referenced")
+
+
 CONTROLLERNAME = 'C-663'
 STAGES = 'L-406.40SD00'  # connect stages to axes
 #REFMODES = 'FPL'
-REFMODES = ('FNL')
-DESTINATION = 120 # mm
+REFMODES = 'FNL'
+DESTINATION = 1 # mm
 
 with GCSDevice(devname = CONTROLLERNAME) as pidevice: 
     # return list of string with the found devices
@@ -36,11 +46,15 @@ with GCSDevice(devname = CONTROLLERNAME) as pidevice:
 
     # initialise the controller and the stages
     pitools.startup(pidevice, stages = STAGES, refmodes = REFMODES)
-
+    
+    # move towards REFMODE and wait until stage is ready on target
+    move_to_ref(pidevice,REFMODES)
+    
     # return values of the minimum and maximum position of the travel range of axis
     rangemin = list(pidevice.qTMN().values())
     rangemax = list(pidevice.qTMX().values())
 
+    # check entries for destination
     try:
         is_valid = pos_in_range(DESTINATION,rangemin[0],rangemax[0])
     except ValueError:
@@ -49,6 +63,9 @@ with GCSDevice(devname = CONTROLLERNAME) as pidevice:
         print("That was no valid type!")
     except InvalidPosInput:
         print("Desired destination is out of range!")
+        
 
+    
+    
 
 
