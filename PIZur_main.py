47a974122077bc147b4358f8b16205b34a646a56
unknown <ophadmin@FR219-LBPC005.personale.dir.unibo.it>
2023-02-10 13:03:07 +0100

    add variables declaration for zhinst

--- PIZur_main.py
+++ PIZur_main.py
@@ -5,19 +5,45 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-CONTROLLERNAME = 'C-663'
-STAGES = 'L-406.40SD00'  # connect stages to axes
-#REFMODES = 'FPL'
-REFMODES = 'FNL'
-scan_edges = [0,10]
-stepsize = 1
-#DIRECTION = "FRWD"
-DIRECTION = "BCWD"
+# ---------------------------------------------------------
+### PI PARAMETERS:
+controller_name = 'C-663'
+stage = 'L-406.40SD00'  # connect stage to axes
+#refmode = 'FPL'
+refmode = 'FNL'
+#direction = "FRWD"
+direction = "BCWD"
 
+### Scan:
+scan_edges = [0,11]
+stepsize = 0.1
+# ---------------------------------------------------------
 
-with GCSDevice(devname = CONTROLLERNAME) as pidevice: 
+# ---------------------------------------------------------
+# ZHINST PARAMETERS:
+device_id = 'dev4910'
+num_grids = 1
+server_host = "169.254.196.174"
+apilevel_example = 6  
+server_port =8004
+
+### Demodulator:
+in_channel = 0          # Input channel V1
+amplitude = 1.0         # sensitivity of the lock-in
+trigger_demod_index = 0 # demodulator 1
+osc_index = 0           # oscillator of demodulator 1 (manual !!)
+demod_rate = 10e3       # demodulator sampling rate: 100 kHz
+demod_order = 4         # filtering order of the demulator
+demod_bandwidth = 10    # bandwidth of the demodulator
+frequency = 100         # frequency of the internal oscillator
+ac_filter = 0           # AC Filter; 1 = activated, 0 = deactivated
+imp50 = 0               # set 50 Ohm impedance at the entrace; 1 = activated, 0 = deactivated
+harmonic = 1            # set multiple of the oscillator frequency for demulating
+# ---------------------------------------------------------
+
+with GCSDevice(devname = controller_name) as pidevice: 
     # return list of string with the found devices
-    devices = pidevice.EnumerateUSB(mask=CONTROLLERNAME)
+    devices = pidevice.EnumerateUSB(mask=controller_name)
     # check that devices is not empty        
     if not devices: 
        raise Exception("There are no connected devices! Please, connect at least one device.")
@@ -32,22 +58,22 @@ with GCSDevice(devname = CONTROLLERNAME) as pidevice:
     pidevice.ConnectUSB(devices[item])
     print('connected: {}'.format(pidevice.qIDN().strip()))
 
-    # initialise the controller and the stages
-    pitools.startup(pidevice, stages = STAGES, refmodes = REFMODES)
+    # initialise the controller and the stage
+    pitools.startup(pidevice, stage = stage, refmode = refmode)
     # move towards REFMODE and wait until stage is ready on target
-    fc.move_to_ref(pidevice,REFMODES)
+    move_stage_to_ref(pidevice,refmode)
     # set trigger output to "In motion"
-    fc.trigger_config(pidevice,axis = 1,type = 6)
+    configure_out_trig(pidevice,axis = 1,type = 6)
     
     # return values of the minimum and maximum position of the travel range of axis
-    ranges = fc.axis_edges(pidevice)
+    ranges = axis_edges(pidevice)
     
     for min,max in ranges:
         # check whether scan edges are within allowed range and sort scan_edges values
-        scan_edges = fc.target_within_range(scan_edges,[min,max])
+        scan_edges = target_within_axis_edges(scan_edges,[min,max])
     
     # define target positions through numpy linspace
-    targets = fc.line_scan_partition(scan_edges,stepsize,DIRECTION)
+    targets = scan1D_partition(scan_edges,stepsize,direction)
     
     # execute the line scan and return the sampled positions
-    positions = fc.line_scan_execution(pidevice,targets)
+    positions = line_scan_execution(pidevice,targets)
