24d391545cb1944268fbab69143915639d3e26d3
unknown <ophadmin@FR219-LBPC005.personale.dir.unibo.it>
2023-02-10 12:49:14 +0100

    add function process_raw_data for zhinst data processing

--- functions.py
+++ functions.py
@@ -2,6 +2,32 @@ from pipython import GCSDevice,pitools, GCS2Commands
 import zhinst.utils
 import numpy as np
 
+def process_raw_data(signal_paths,raw_data,index):
+    """Gets and process raw_rata and updates data value
+
+    Parameters
+    ----------
+    signal_paths : list
+        list of strings of the subscribed nodes
+    raw_data : dict
+        dictionary in which read data are stored
+    print_cols : index
+        index : int
+        number that contains the number of step of the scan so far
+
+    Returns
+    -------
+    _ : float
+        value that contains the average over the columns
+    """
+
+    for i, signal_path in enumerate(signal_paths):
+        for signal_burst in raw_data.get(signal_path.lower(),[]):
+            # skip the first two steps (required for calibration)
+            if index >= 2:
+                value = signal_burst["value"][index-2,:]
+                return np.mean(value)
+
 def move_stage_to_ref(pidevice,refmode):
     """Moves the stage towards the selected reference point
 
