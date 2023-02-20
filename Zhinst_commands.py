f98a7d79802b333cfbc9b1622c83648e55fe3d45
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-02-20 15:46:30 +0100

    Fixed some bugs in Zhist_command for automated daq setting

--- Zhinst_commands.py
+++ Zhinst_commands.py
@@ -15,28 +15,23 @@ class zhinst_lockin:
         self.daq.setDebugLevel(6)
         zhinst.utils.disable_everything(self.daq,self.device)
         
-          
     def input_signal_settings(self,in_channel,input_sig_pars):
         """upload on the lock-in all the settings for the input signal acquisition"""
-        for feature,value in input_sig_pars.items():
-
-            setting = "/%s/sigins/%d/%s"% (self.device_id,in_channel,feature),value
-            self.daq.set(setting)
+        for feature,value in input_sig_pars.items(): 
+            self.daq.set("/%s/sigins/%d/%s"% (self.device_id,in_channel,feature),value)
         
     def demod_signal_settings(self,trigger_demod_index,demod_pars):
         """upload on the lock-in all the settings for the demodulators"""
         for feature,value in demod_pars.items():
             if feature != 'bandwidth':
-                setting = "/%s/demods/%d/%s" % (self.device_id,trigger_demod_index,feature),value
-                self.daq.set(setting)
+                self.daq.set("/%s/demods/%d/%s" % (self.device_id,trigger_demod_index,feature),value)
         
         timeconstant = zhinst.utils.bw2tc(demod_pars["bandwidth"], demod_pars["order"])
-        set_time = "/%s/demods/%d/%s" % (self.device_id,trigger_demod_index,"timeconstant"),timeconstant
-        self.daq.setDouble(set_time)
+        self.daq.setDouble("/%s/demods/%d/%s" % (self.device_id,trigger_demod_index,"timeconstant"),timeconstant)
         # Wait for the demodulator filter to settle.
-        timeconstant_set = self.aq.getDouble(
+        timeconstant_set = self.daq.getDouble(
             "/%s/demods/%d/timeconstant" % (self.device, trigger_demod_index)
-                                        )
+                                            )
         time.sleep(10 * timeconstant_set)
         # perform a global synchronisation between the device and the data server")
         self.daq.sync()
@@ -50,7 +45,7 @@ class zhinst_lockin:
         """upload on the lock-in all the settings for data acquisition"""
         self.daq_module = self.daq.dataAcquisitionModule()
         for feature,value in data_acquisition_pars.items():
-            self.daq_module(feature,value)
+            self.daq_module.set(feature,value)
     
     def subscribe_to_signals(self,signal_paths):
         self.data = {}
