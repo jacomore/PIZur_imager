05a3b671f3733857016c8f9aaa4c946ec4ae1705
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-02-22 18:00:35 +0100

    removed lines for plotting

--- PIZur_main.py
+++ PIZur_main.py
@@ -6,23 +6,9 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 import json
 
-def update(data):
-    """Update frame for plotting"""
-    ydata.append(data[0][0])
-    xdata.append(data[1][0])
-    ln.set_data(xdata,ydata)
-    return ln,
-
-
 if __name__ == "__main__":   
     # setup instruments 
     scanner = Scan1D('input_dicts.json')
-
-     # setup two dimensional plots
-    fig, ax = plt.subplots()
-    xdata, ydata = [],[]
-    ln, = ax.plot([],[], 'ro')
-    ani = FuncAnimation(fig, update, frames = scanner.execute_1D_scan(), interval = 10, blit=True, fargs = (scanner,),cache_frame_data = False)
     
-
-    
+    # execute scan 1D
+    scanner.execute_1D_scan()
