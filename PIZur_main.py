3a424f7d470d625056f5b54ac20eb6e062991461
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-02-23 20:14:30 +0100

    tested scan_2D class in continous sampling

--- PIZur_main.py
+++ PIZur_main.py
@@ -1,14 +1,38 @@
-from Scanner_classes import Scan1D
-from multiprocess import Pipe, Process
-from pipython import pitools
-import matplotlib.pyplot as plt
+from Scanner_classes import Scan_1D, Scan_2D
 from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
 import numpy as np
-import json
 
-if __name__ == "__main__":   
-    # setup instruments 
-    scanner = Scan1D('input_dicts.json')
+def init():
+    """Initialise the AxesImage object (image attached to an axis). 
     
-    # execute scan 1D
-    scanner.execute_1D_scan()
+    Returns:
+        im, : AxesImage object that represents the image    
+    """
+    im.set(animated = True, clim = (0,AMPLITUDE),cmap = 'magma',interpolation = None,)
+    return im,
+
+def updatefig(InData):
+    line = InData[0]
+    row_index = InData[1]
+    print(line)
+    print(type(line))
+    data[row_index,:] = line[0][:]
+    im.set_data(data)
+    return im,
+
+if __name__ == '__main__':
+
+    scanner = Scan_2D('input_dicts.json')
+
+    fig, ax = plt.subplots()
+    X_MIN , X_MAX = 0 , 499
+    Y_MIN, Y_MAX = 0 , 499
+    STEPX , STEPY = 0.01,0.01
+    DIMX, DIMY = 500 , 500 
+    AMPLITUDE = 3
+    data = np.empty((DIMX,DIMY))
+    im = plt.imshow(data)
+    
+    ani = FuncAnimation(fig, updatefig, frames = scanner.execute_2D_continous_scan,init_func=init, interval = 30, blit=True,cache_frame_data = False)
+    plt.show()
