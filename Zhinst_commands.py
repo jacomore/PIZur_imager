fbea726e48466a8630974e36c111f1e00a6d5c43
Giacomo Rizzi - giacomo.rizzi5@studio.unibo.it <giacomo.rizzi5@studio.unibo.it>
2023-02-26 12:35:08 +0100

    added method 'timeconstant_setting' and 'evaluate_nrows' for increased readibility

--- Zhinst_commands.py
+++ Zhinst_commands.py
@@ -10,7 +10,7 @@ class zhinst_lockin:
     """
     def __init__(self,device_pars):
         self.device_id = device_pars["device_id"]
-        self.daq, self.device, self.props = zhinst.utils.create_api_session(device_pars["device_id"],
+        self.daq, self.device, props = zhinst.utils.create_api_session(device_pars["device_id"],
                                                     device_pars["apilevel"], 
                                                     device_pars["server_host"],
                                                     device_pars["server_port"])
@@ -26,21 +26,22 @@ class zhinst_lockin:
                 self.daq.set("/%s/sigins/%d/%s"% (self.device_id,in_channel,feature),value)
         
     def demod_signal_settings(self,demod_pars):
-        trigger_demod_index = demod_pars["trigger_demod_index"]
         """upload on the lock-in all the settings for the demodulators"""
         for feature,value in demod_pars.items():
             if feature not in ['bandwidth','trigger_demod_index'] :
                 print("Setting ", feature, "to ",value)
-                self.daq.set("/%s/demods/%d/%s" % (self.device_id,trigger_demod_index,feature),value)
+                self.daq.set("/%s/demods/%d/%s" % (self.device_id, demod_pars["trigger_demod_index"],feature),value)
 
+    def timeconstant_setting(self,demod_pars):
+        """upload on the lock-in the setting for the timeconstant"""
         timeconstant = zhinst.utils.bw2tc(demod_pars["bandwidth"], demod_pars["order"])
         print("Setting timeconstant to ",timeconstant)
-        self.daq.setDouble("/%s/demods/%d/%s" % (self.device_id,trigger_demod_index,"timeconstant"),timeconstant)
+        self.daq.setDouble("/%s/demods/%d/%s" % (self.device_id,demod_pars["trigger_demod_index"],"timeconstant"),timeconstant)
         # Wait for the demodulator filter to settle.
-        timeconstant_set = self.daq.getDouble(
-            "/%s/demods/%d/timeconstant" % (self.device, trigger_demod_index)
+        timeconstant_settler = self.daq.getDouble(
+            "/%s/demods/%d/timeconstant" % (self.device,  demod_pars["trigger_demod_index"])
                                             )
-        time.sleep(10 * timeconstant_set)
+        time.sleep(10 * timeconstant_settler)
         # perform a global synchronisation between the device and the data server")
         self.daq.sync()
     
@@ -48,13 +49,36 @@ class zhinst_lockin:
         """ upload on the lock-in the setting for the oscillator channel"""
         print("Setting frequency to ",osc_pars["osc_freq"])
         self.daq.set("/%s/oscs/%d/freq" % (self.device_id, osc_pars["osc_index"]), osc_pars["osc_freq"])
+    
+    def evaluate_nrows(self,scan_pars):
+        """ evaluate the number of rows of the scan depending on the scan type"""
+        if scan_pars["dim"] == 1: 
+            if scan_pars["type"] == "continous":
+                return 1
+            elif scan_pars["type"] == "discrete":
+                return int(np.floor((scan_pars["scan_edges"][1]-scan_pars["scan_edges"][0])/scan_pars["stepsize"])) + 3  # (1 + 2 = 3, 2 for calibration)
+            else: 
+                raise Exception("Type of scan in input is not valid! Type either continous or discrete.") 
             
-    def join_data_acquisition_pars(self,data_acquisition_pars,demod_pars,PI):
+        elif scan_pars["dim"] == 2: 
+            if scan_pars["type"] == "continous":
+                return int(np.floor((scan_pars["servo_scan_edges"][1]-scan_pars["servo_scan_edges"][0])/scan_pars["servo_stepsize"] + 3))   # servo is responsible for discrete motion along lines
+            elif scan_pars["type"] == "discrete":
+                pixel_per_line = int(np.floor((scan_pars["scan_edges"][1]-scan_pars["scan_edges"][0])/scan_pars["stepsize"])) + 1
+                number_of_line = int(np.floor((scan_pars["servo_scan_edges"][1]-scan_pars["servo_scan_edges"][0])/scan_pars["servo_servo_stepsize"])) + 1
+                nrows = pixel_per_line*number_of_line + 2 # + 2 for calibration porpuses
+                return nrows
+            else: 
+                raise Exception("Type of scan in input is not valid! Type either continous or discrete.") 
+        else:
+            raise Exception("Scan dimension parameter (dim) in input is not valid! Type either continous or discrete.") 
+    
+    def join_data_acquisition_pars(self,data_acquisition_pars,demod_pars,scan_pars):
         """Evaluate some parameters for the data acquisition that are deduced from the 
            other input parameters of the demodulator and of the scan."""
         burst_duration = data_acquisition_pars["duration"]
         num_cols = int(np.ceil(demod_pars["rate"]* burst_duration))  
-        num_rows = int((PI["master_scan_edges"][1]-PI["master_scan_edges"][0])/PI["stepsize"] + 3)  
+        num_rows = self.evaluate_nrows(scan_pars)
         triggernode = "/%s/demods/%d/sample.TrigIn1" % (self.device_id,demod_pars["trigger_demod_index"])
         dir_to_save = os.getcwd()+"\\"+"Results"
 
