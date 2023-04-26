e072fbaf30bdd661ee8805fb7864bb4a52396397
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-04-26 18:38:30 +0200

    Add docstrings to input and output processors module!

--- InputProcessor.py
+++ InputProcessor.py
@@ -1,62 +1,149 @@
 import numpy as np
 
 class InputProcessor():
-    """Return values to be provided to the Zurich lockin """
-    def __init__(self,scan_pars,sampling_rate):
-        self.scan_pars = scan_pars
-        self.s_rate = sampling_rate
-
-    def process_1d_input(self):
-        """Process input data for 1D scan in order to find the number of 
-           rows, columns and the duration of the triggered data acquisition of the zurich lock-in"""
-        scan_edges = self.scan_pars["scan_edges"]
-        stepsize = self.scan_pars["stepsize"]
-        acc = self.scan_pars["acceleration"]
-        vel = self.scan_pars["velocity"]
-        delta = abs(scan_edges[1]-scan_edges[0])
-
-        if self.scan_pars["type"] == "continous":
-            
-            N_cols = int(np.floor(delta/stepsize)) + 1
-            N_rows = 1
-            duration = self.duration_calculator(acc,delta,vel)
-            daq_pars =  {
-                        "daq_columns" : N_cols,
-                        "daq_rows" : N_rows,
-                        "duration" : duration,
-                        "mode" : "Linear",
-                        "trigger type" : "HW trigger",
-                        "trigger edge" : "positive",
-                        "holdoff" : duration*(0.95),
-                        "out_columns" : N_cols,
-                        "out_rows" : N_rows
-                        }
-            
-        else:
-
-            N_rows = int(np.floor(delta/stepsize)) + 1
-            duration = 0.05
-            N_cols = int(np.floor(duration* self.s_rate))
-            daq_pars =  {
-                        "daq_columns" : N_cols,
-                        "daq_rows" : N_rows,
-                        "duration" : duration,
-                        "mode" : "Exact (on-grid)",
-                        "trigger type" : "HW trigger",
-                        "trigger edge" : "negative",
-                        "holdoff" : duration*(0.95),
-                        "out_columns" : N_cols,
-                        "out_rows" : N_rows
-                        }
-        return daq_pars
+    """A class to process input data for the Zurich lock-in"""
     
+    def __init__(self,scan_pars):
+        """
+        Initialize an InputProcessor instance.
 
-    def duration_calculator(self,acc,delta,vel):
-            if (np.sqrt(acc*delta)>vel):
-                duration = vel/acc + delta/vel
-            else:
-                duration = np.sqrt(delta/acc)
-            return duration
+        Parameters:
+        -----------
+        scan_pars : dict
+            A dictionary containing scan parameters, including:
+                - scan_edges : list of two floats
+                    The edges of the scan range.
+                - stepsize : float
+                    The step size for the scan.
+                - acceleration : float
+                    The acceleration of moving stage scans.
+                - velocity : float
+                    The velocity of the moving stage scans.
+                - sampling_frequency : float
+                    The sampling frequency used for discrete scans.
+                - type : str
+                    The type of scan, either 'continuous' or 'discrete'.
+        """
+        self.scan_edges = scan_pars["scan_edges"]
+        self.stepsize = scan_pars["stepsize"]
+        self.acc = scan_pars["acceleration"]
+        self.vel = scan_pars["velocity"]
+        self.sampl_freq = scan_pars["sampling_frequency"]
+        self.type = scan_pars["type"]
+    
+    def process_continuous_input(self,delta):
+            """
+            Process input data to find the number of rows and columns
+            for a continuous scan with PI controller and Zurich lock-in.
+
+            Parameters:
+            -----------
+            delta : float
+                The distance between the scan edges.
+
+            Returns:
+            --------
+            N_cols : int
+                The number of columns for the data acquisition.
+            N_rows : int
+                The number of rows for the data acquisition.
+            """
+            N_rows = 1
+            N_cols = int(np.floor(delta/self.stepsize)) + 1
+            return N_cols, N_rows
+    
+    def process_discrete_input(self,delta):
+        """
+        Process input data to find the number of rows and columns
+        for a discrete scan with Pi controller and Zurich lock-in
+
+        Parameters:
+        -----------
+        delta : float
+            The distance between the scan edges.
+
+        Returns:
+        --------
+        N_cols : int
+            The number of columns for the data acquisition.
+        N_rows : int
+            The number of rows for the data acquisition.
+        """
+        N_rows = int(np.floor(delta/self.stepsize)) + 1
+        N_cols = int(np.floor(0.05*self.sampl_freq))
+        return N_cols, N_rows
+
+    def process_input(self):
+        """
+        Process input data for a 1D scan to find the number of rows, columns,
+        and the duration of the triggered data acquisition for the Zurich lock-in.
+
+        Returns:
+        --------
+        daq_pars : dict
+            A dictionary containing data acquisition parameters, including:
+                - daq_columns : int
+                    The number of columns for the data acquisition.
+                - daq_rows : int
+                    The number of rows for the data acquisition.
+                - duration : float
+                    The duration of the triggered data acquisition.
+                - mode : str
+                    The data acquisition mode; 'Exact (on-grid)' for discrete scan, 'Linear' for continuous
+                - trigger type : str
+                    The trigger type, always set to 'HW trigger'.
+                - trigger edge : str
+                    The trigger edge; 
+                - holdoff : float
+                    The holdoff time for the trigger
+        """
+        delta = abs(self.scan_edges[1]-self.scan_edges[0])
+
+        if self.type == "continuous":
+            duration = self.duration_calculator(delta)
+            N_cols, N_rows = self.process_continuous_input(delta)
+            mode = "Linear"
+            edge = "positive"
+        else:
+            duration = 0.05 # 50 milliseconds
+            N_cols, N_rows = self.process_discrete_input(delta)
+            mode = "Exact (on-grid)"
+            edge = "negative"
+            
+        daq_pars =  {
+                    "daq_columns" : N_cols,
+                    "daq_rows" : N_rows,
+                    "duration" : duration,
+                    "mode" : mode,
+                    "trigger type" : "HW trigger",
+                    "trigger edge" : edge,
+                    "holdoff" : duration*(0.95),
+                    }
+        return daq_pars
+
+    def duration_calculator(self,delta):
+        """
+        Calculate the duration of the triggered data acquisition for a continuous scan.
+
+        Parameters:
+        -----------
+        acc : float
+            The acceleration of the moving stage scans.
+        delta : float
+            The distance between the scan edges.
+        vel : float
+            The velocity of the moving stage scans.
+
+        Returns:
+        --------
+        duration : float
+            The duration of the triggered data acquisition.
+        """
+        if (np.sqrt(self.acc*delta)>self.vel):
+            duration = self.vel/self.acc + delta/self.vel
+        else:
+            duration = np.sqrt(delta/self.acc)
+        return duration
 
             
         
--- OutputProcessor.py
+++ OutputProcessor.py
@@ -1,43 +1,85 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
 
 class OutputProcessor():
+
     def __init__(self,filename,scan_pars,daq_pars):
-        # parameters obtained in input by input_dicts
+        """
+        Initialize an OutputProcessor object with the given filename, scan parameters, and DAQ parameters.
+
+        Args:
+        - filename (str): the name of the file containing the output data
+        - scan_pars (dict): a dictionary containing the scan parameters
+        - daq_pars (dict): a dictionary containing the DAQ parameters
+
+        Attributes:
+        - scan_pars (dict): a dictionary containing the scan parameters
+        - filename (str): the name of the file containing the output data
+        - daq_pars (dict): a dictionary containing the DAQ parameters
+        - N_rows (int): the number of rows in the DAQ data
+        - N_cols (int): the number of columns in the DAQ data
+        """
         self.scan_pars = scan_pars
-        # filename that contains the output data
         self.filename = filename
-        # parameters obtained by the daq
         self.daq_pars = daq_pars
-        self.N_rows = daq_pars["out_rows"]
-        self.N_cols = daq_pars["out_columns"]
+        self.N_rows = daq_pars["daq_columns"]
+        self.N_cols = daq_pars["daq_rows"]
 
     def get_raw_data(self):
-        """read raw_data as a Numpy array and return only the third column, 
-           containing the pixels in a column """
+        """
+        Read raw data from the file and return the third column, containing the pixels in a column.
+
+        Returns:
+        - raw_data (ndarray): a NumPy array containing the third column of the input file
+        """
         raw_data = np.genfromtxt(self.filename,skip_header = 1, delimiter = ";")
         return raw_data[:,2] # third column contains all the data in a column     
     
     def evaluate_averaged_data(self,raw_data): 
-        """average the input data in case of a 1D discrete scan"""
+        """
+        Average the input data in case of a 1D discrete scan.
+
+        Args:
+        - raw_data (ndarray): a NumPy array containing the raw data
+
+        Returns:
+        - avg_data (ndarray): a NumPy array containing the averaged data
+        """
         avg_data = np.empty(self.N_rows)
         for row in range(self.N_rows):
             avg_data[row] = np.mean(raw_data[row*self.N_cols:(row + 1)*self.N_cols])
         return avg_data
     
     def evaluate_target_positions(self,scanedges,stepsize):
-        """ Evaluate the partition of the target points for a 1D scan   
-        """ 
+        """
+        Evaluate the partition of the target points for a 1D scan.
+
+        Args:
+        - scanedges (list): a list containing the start and end positions of the scan
+        - stepsize (float): the step size of the scan
+
+        Returns:
+        - targets (ndarray): a NumPy array containing the target positions
+        """
         # calculate targets points
         Npoints = int((scanedges[1]-scanedges[0])/stepsize) + 1
         return np.linspace(scanedges[0],scanedges[1],Npoints,endpoint=  True)
 
-    def save_1D_data_file(self,targets,avg_data):
+    def save_1D_data_file(self,targets,avg_data):         
+        """
+        Save the cleaned 1D data to a file.
+
+        Args:
+        - targets (ndarray): a NumPy array containing the target positions
+        - avg_data (ndarray): a NumPy array containing the averaged data
+        """
         out_name = "cleaned_1D_data.txt"
         out_file = np.column_stack((targets,avg_data))
         np.savetxt(out_name, out_file, fmt = "%10.6f", delimiter = ",")
 
     def process_1D_data(self):
+        """
+        Process the 1D data and save it to a file.
+        """
         # get targets position
         scanedges = self.scan_pars["scan_edges"]
         stepsize = self.scan_pars["stepsize"]
@@ -48,7 +90,7 @@ class OutputProcessor():
             out_data = self.evaluate_averaged_data(raw_data, )
         else: 
             out_data = raw_data
-        # save data and image
+        # save data
         self.save_1D_data_file(targets,out_data)
 
             
