77b42ef1c22d962ff81d39116b4b2c5a0d6deaa8
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-04-26 16:58:22 +0200

    Removed functionalities for StepperChain (2D)

--- PI_commands.py
+++ PI_commands.py
@@ -1,6 +1,5 @@
 from pipython import GCSDevice, pitools, GCS2Commands
 
-
 class Stepper:
     """Represents an axis connected to a PI controller.
 
@@ -116,74 +115,5 @@ class Stepper:
         self.pidevice.CTO(1, 3, trigger_type)
         # enable trigger output with the configuration defined above
         self.pidevice.TRO(1, True)
-        
-        
-class StepperChain:
-    """Handles the connection with two pidevices, making use of the Stepper class
-    to control a USB daisy chain of two devices.
-
-    Attributes
-    ----------
-    controller_id : str
-        The ID of the controller.
-    axis_id : int
-        The ID of the axis.
-
-    Methods
-    -------
-    connect_daisy(dev_indices) 
-        Connects master and servo to form a daisy chain.
-    reference_both_stages(ref_modes)
-        References both stages.
-    configure_both_trig(trigger_types)
-        Configures the output trigger modes of the two devices.
-    """
-
-    def __init__(self, controller_id: str, axis_id: int):
-        """Initializes the StepperChain class.
-
-        Parameters:
-        controller_id (str): The ID of the controller.
-        axis_id (int): The ID of the axis.
-        """
-        self.controller_id = controller_id
-        self.axis_id = axis_id
-        self.master = Stepper(controller_id, axis_id)
-        self.servo = Stepper(controller_id, axis_id)
-
-    def connect_daisy(self, dev_indices) -> None:
-        """Connects master and servo to form a daisy chain.
-
-        Parameters:
-        dev_indices (List[int]): List of two integer numbers defining the connection
-            of the two devices (1 = master, 2 = Servo).
-        """
-        devices = self.master.usb_plugged_devices()
-        self.master.pidevice.OpenUSBDaisyChain(description=devices[0])
-        daisy_chain_id = self.master.pidevice.dcid
-        self.master.pidevice.ConnectDaisyChainDevice(dev_indices[0], daisy_chain_id)
-        pitools.startup(self.master.pidevice, self.master.axis_id)
-        self.servo.pidevice.ConnectDaisyChainDevice(dev_indices[1], daisy_chain_id)
-        pitools.startup(self.servo.pidevice, self.servo.axis_id)
-
-    def reference_both_stages(self, ref_modes) -> None:
-        """References both stages.
-
-        Parameters:
-        ref_modes (List[str]): List of two strings defining the referencing modes.
-        """
-        self.master.move_stage_to_ref(ref_modes[0])
-        self.servo.move_stage_to_ref(ref_modes[1])    
-    
-    def configure_both_trig(self, trigger_types) -> None:
-        """Configures the output trigger modes of the two devices.
-
-        Parameters:
-        trigger_types (List[int]): List of integers defining the types of trigger to be output
-            (6 == in Motion, 1 = Line trigger); first element is associated with master,
-            second with servo.
-        """
-        self.master.configure_out_trigger(trigger_types[0])
-        self.servo.configure_out_trigger(trigger_types[1])
         
     
