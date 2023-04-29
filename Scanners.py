4d9edfde7a77d8eefe98abd85f235f833ce9fccd
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-04-29 19:05:50 +0200

    Changed the architecture of the program to have more control on the single methods.

--- Scanners.py
+++ Scanners.py
@@ -6,32 +6,27 @@ class Scan1D:
     def __init__(self,InPars):    
         self.PI = InPars["pi"]        
         self.scan_pars = InPars["scan_pars"]
-
-        # connect 1D device
-        self.connect_1D_PI()   
-        # set properties of the motion and of the devices
-        self.setup_1D_PI()
-
         self.scan_edges = self.scan_pars["scan_edges"]
         self.stepsize = self.scan_pars["stepsize"]
-        self.evaluate_target_positions()
-
-    def connect_1D_PI(self):
-        """ perform all the procedures for setting up properly the PI device""" 
+        # evaluate target positions for the 1D scan
+        self.targets = self.evaluate_target_positions()
         self.master = Stepper(self.PI["ID"],self.PI["stage_ID"]) 
-        self.master.connect_pidevice()
-    
-    def setup_1D_PI(self):
-        """Set the parameters of interest in the ROM of the device"""
-        self.master.set_velocity(self.scan_pars["velocity"])
-        self.master.set_acceleration(self.scan_pars["acceleration"])
 
     def evaluate_target_positions(self):
         """ Evaluate the partition of the target points for a 1D scan   
         """ 
         # calculate targets points
         Npoints = int(abs(self.scan_edges[1]-self.scan_edges[0])/self.stepsize) + 1
-        self.targets =  np.linspace(self.scan_edges[0],self.scan_edges[1],Npoints,endpoint=  True)
+        return np.linspace(self.scan_edges[0],self.scan_edges[1],Npoints,endpoint=  True)
+
+    def connect_master(self):
+        """ perform all the procedures for setting up properly the PI device""" 
+        self.master.connect_pidevice()
+    
+    def setup_master(self):
+        """Set the parameters of interest in the ROM of the device"""
+        self.master.set_velocity(self.scan_pars["velocity"])
+        self.master.set_acceleration(self.scan_pars["acceleration"])
 
     def init_1D_scan(self):
         """ Setup the 1D scan by: (1) moving the axis on all the targets positions, 
@@ -45,15 +40,17 @@ class Scan1D:
         """ Execute the 1D scan by: (1) moving the axis on all the targets positions, 
             (2) measuring the raw_data from the Zurich lock-in (3) saving the data on read
         """
-        self.init_1D_scan()
+        cur_pos = []
         for target in self.targets:
             self.master.move_stage_to_target(target)        
             print("Position: ", target)
+            cur_pos.append(self.master.get_curr_pos())
+        return cur_pos
+
     
     def execute_continuous_1D_scan(self):
         """ Execute the 1D scan by: (1) moving the axis on all the targets positions, 
             (2) measuring the raw_data from the Zurich lock-in (3) moving continously
         """
-        self.init_1D_scan()
         self.master.move_stage_to_target(self.targets[-1])        
         
