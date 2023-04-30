ed90c9985cdb54465c5ea522553d2531e2aca247
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-04-30 11:04:22 +0200

    renamed master into stepper and modified names of the functions for sake of clarity

--- Scanners.py
+++ Scanners.py
@@ -10,7 +10,7 @@ class Scan1D:
         self.stepsize = self.scan_pars["stepsize"]
         # evaluate target positions for the 1D scan
         self.targets = self.evaluate_target_positions()
-        self.master = Stepper(self.PI["ID"],self.PI["stage_ID"]) 
+        self.stepper = Stepper(self.PI["ID"],self.PI["stage_ID"]) 
 
     def evaluate_target_positions(self):
         """ Evaluate the partition of the target points for a 1D scan   
@@ -19,38 +19,43 @@ class Scan1D:
         Npoints = int(abs(self.scan_edges[1]-self.scan_edges[0])/self.stepsize) + 1
         return np.linspace(self.scan_edges[0],self.scan_edges[1],Npoints,endpoint=  True)
 
-    def connect_master(self):
+    def connect_stepper(self):
         """ perform all the procedures for setting up properly the PI device""" 
-        self.master.connect_pidevice()
+        self.stepper.connect_pidevice()
     
-    def setup_master(self):
+    def setup_motion_stepper(self):
         """Set the parameters of interest in the ROM of the device"""
-        self.master.set_velocity(self.scan_pars["velocity"])
-        self.master.set_acceleration(self.scan_pars["acceleration"])
+        self.stepper.set_velocity(self.scan_pars["velocity"])
+        self.stepper.set_acceleration(self.scan_pars["acceleration"])
 
-    def init_1D_scan(self):
+    def init_stepper_scan(self):
         """ Setup the 1D scan by: (1) moving the axis on all the targets positions, 
             (2) measuring the raw_data from the Zurich lock-in (3) saving the data on read
         """
-        self.master.move_stage_to_ref(self.PI["refmode"])
-        self.master.move_stage_to_target(self.targets[0])
-        self.master.configure_out_trigger(trigger_type=6)
+        # high default values to obtain a quick referencing 
+        self.stepper.set_velocity(10)
+        self.stepper.set_acceleration(20)
+    
+        self.stepper.move_stage_to_ref(self.PI["refmode"])
+        self.stepper.move_stage_to_target(self.targets[0])
+        trigtype = self.PI["trig_type"]
+        self.stepper.configure_out_trigger(trigger_type=trigtype)
 
-    def execute_discrete_1D_scan(self):
+    def execute_discrete_scan(self):
         """ Execute the 1D scan by: (1) moving the axis on all the targets positions, 
             (2) measuring the raw_data from the Zurich lock-in (3) saving the data on read
         """
         cur_pos = []
         for target in self.targets:
-            self.master.move_stage_to_target(target)        
+            self.stepper.move_stage_to_target(target)        
             print("Position: ", target)
-            cur_pos.append(self.master.get_curr_pos())
+            cur_pos.append(self.stepper.get_curr_pos())
         return cur_pos
 
     
-    def execute_continuous_1D_scan(self):
+    def execute_continuous_scan(self):
         """ Execute the 1D scan by: (1) moving the axis on all the targets positions, 
             (2) measuring the raw_data from the Zurich lock-in (3) moving continously
         """
-        self.master.move_stage_to_target(self.targets[-1])        
+        self.stepper.move_stage_to_target(self.targets[-1])        
         
