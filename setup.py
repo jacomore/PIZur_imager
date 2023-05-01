54d2f5179ab06f3f7b80ead423ea866793df5720
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-05-01 14:54:53 +0200

    another test for pypi
    github.ref=refs/tags/v0.1.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="pizurscan",
-    version="0.1.2",
+    version="0.1.3",
     description="Library to interface PI controllers and Zurich lock-in",
     long_description=long_description,
     long_description_content_type="text/markdown",
