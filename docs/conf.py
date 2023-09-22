53dd01ddd75f9de4c1add672ee9bbd7a082661b5
Giacomo Rizzi <rizzigiacomo@pm.me>
2023-09-22 10:05:54 +0200

    upgrade to new version

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'pizurscan'
 copyright = '2023, Giacomo Rizzi'
 author = 'Giacomo Rizzi'
-release = '0.1.5'
+release = '0.1.6'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
